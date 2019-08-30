af45ef3d5f2de3188dc94fcf50f7ab4b1e3ef167
Timothée Corsini <timothee.corsini@gmail.com>
2019-08-30 20:31:10 +0200

    fonctions statiques

--- vector3d.cpp
+++ vector3d.cpp
@@ -7,3 +7,35 @@ Vector3D::Vector3D() {
 
 Vector3D::~Vector3D() {
 }
+
+/* Methodes statiques */
+
+Vector3D operator* (double k, const Vector3D& v) {
+  return v*k;
+}
+
+double scalar(const Vector3D& v, const Vector3D& u) {
+  return v.scalar(u);
+}
+
+double norm(const Vector3D& v) {
+  return v.norm();
+}
+
+Vector3D normalize(const Vector3D& v) {
+  return v.normalize();
+}
+
+Vector3D cross(const Vector3D& v, const Vector3D& u) {
+  return v.cross(u);
+}
+
+double distance(const Vector3D& v, const Vector3D& u) {
+  return v.distance(u);
+}
+
+double tripleProduct(const Vector3D& v1,
+		     const Vector3D& v2,
+		     const Vector3D& v3) {
+  return v1.tripleProduct(v2, v3);
+}
--- vector3d.hpp
+++ vector3d.hpp
@@ -9,43 +9,59 @@ namespace m_engine {
   public:
     Vector3D(double x=0, double y=0, double z=0);
     ~Vector3D();
+    
+    //GETTER
+    double operator[] (int i) const;
 
-	//GETTER
-	double operator[] (int i) const;
+    //SETTER
+    double& operator[] (int i);
 
-	//SETTER
-	double& operator[] (int i);
+    //OPERATORS OVERRIDE
+    Vector3D operator+(const Vector3D& u) const;
+    Vector3D& operator+=(const Vector3D& u);
 
-	//OPERATORS OVERRIDE
-	Vector3D operator+(const Vector3D& u) const;
-	Vector3D& operator+=(const Vector3D& u);
+    Vector3D operator-(const Vector3D& u) const;
+    Vector3D& operator-=(const Vector3D& u);
 
-	Vector3D operator-(const Vector3D& u) const;
-	Vector3D& operator-=(const Vector3D& u);
+    Vector3D operator* (double k) const;
+    Vector3D& operator*=(double k);
 
-	Vector3D operator* (double k) const;
-	Vector3D& operator*=(double k);
+    Vector3D operator* (const Vector3D& u) const;
+    Vector3D& operator*=(const Vector3D& u);
 
-	Vector3D operator* (const Vector3D& u) const;
-	Vector3D& operator*=(const Vector3D& u);
+    bool operator==(const Vector3D& u) const;
+    bool operator!=(const Vector3D& u) const; 
 
-	bool operator==(const Vector3D& u) const;
-	bool operator!=(const Vector3D& u) const; 
+    operator std::string() const;
 
-	operator std::string() const;
-
-	//BASIC OPERATIONS ON VECTORS
-	double scalar(const Vector3D& u) const;
-	double norm() const;
-	Vector3D normalize() const;
-	Vector3D cross(const Vector3D& u) const;
-	double distance(const Vector3D& u) const;
-	double tripleProduct(const Vector3D& u, const Vector3D& v) const;
+    //BASIC OPERATIONS ON VECTORS
+    double scalar(const Vector3D& u) const;
+    double norm() const;
+    Vector3D normalize() const;
+    Vector3D cross(const Vector3D& u) const;
+    double distance(const Vector3D& u) const;
+    double tripleProduct(const Vector3D& u, const Vector3D& v) const;
 
 
   private:
-	double coord[3];
+    double coord[3];
   };
+
+  inline Vector3D operator* (double k, const Vector3D& v);
+  
+  inline double scalar(const Vector3D& v, const Vector3D& u);
+  
+  inline double norm(const Vector3D& v);
+  
+  inline Vector3D normalize(const Vector3D& v);
+  
+  inline Vector3D cross(const Vector3D& v, const Vector3D& u);
+  
+  inline double distance(const Vector3D& v, const Vector3D& u);
+  
+  inline double tripleProduct(const Vector3D& v1,
+			      const Vector3D& v2,
+			      const Vector3D& v3);
 
 }
 
