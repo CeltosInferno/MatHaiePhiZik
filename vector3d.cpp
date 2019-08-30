0395e8a3a752174e01010031184908e2c4f5895e
nathan barloy <nbarloy@orange.fr>
2019-08-30 12:10:37 -0400

    constructeur et normalisation

--- vector3d.cpp
+++ vector3d.cpp
@@ -1,9 +1,33 @@
 #include "vector3d.hpp"
 
+#include "cmath"
+
 using namespace m_engine;
 
-Vector3D::Vector3D() {
+Vector3D::Vector3D(double x = 0, double y = 0, double z = 0) {
+	coord[0] = x;
+	coord[1] = y;
+	coord[2] = z;
 }
 
 Vector3D::~Vector3D() {
+}
+
+
+
+
+double Vector3D::norm() const {
+	double s = 0;
+	for (double x : coord)
+	{
+		s += x * x;
+	}
+
+	return std::sqrt(s);
+}
+
+Vector3D Vector3D::normalize() const {
+	double norm = this->norm();
+	Vector3D normVec(coord[0] / norm, coord[1] / norm, coord[2] / norm);
+	return normVec;
 }
