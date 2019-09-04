c424a46b25af3de272cfc6f13134dfbd8d919075
LadyCookie <bouzerea@etu.insa-toulouse.fr>
2019-09-04 15:26:10 -0400

    correction Vector3D

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ using namespace m_engine;
 int main() {
 
   //mieux vaut redefinir un operateur <<
-  std::cout << "potatoe " << (std::string) Vector3D(0,2) << std::endl;
+  std::cout << "potatoe " << Vector3D(0,2) << std::endl;
   return 0;
 }
--- vector3d.cpp
+++ vector3d.cpp
@@ -5,34 +5,37 @@
 
 using namespace m_engine;
 
-Vector3D::Vector3D(double x, double y, double z) {
-  m_coord[0] = x;
-  m_coord[1] = y;
-  m_coord[2] = z;
-}
+Vector3D::Vector3D(double x, double y, double z): x(x), y(y), z(z) {}
 
-Vector3D::~Vector3D() {
-}
+Vector3D::~Vector3D() {}
 
 //GETTER
 double Vector3D::operator[](int i) const {
-  if (i >= 0 && i <= 2) {
-    return m_coord[i];
-  }
-  else {
-    std::cerr << "Error : can't acces to a dimension over 3 or below 0" << i << std::endl;
-    return 0;
-  }
-}
+	switch (i) {
+	case 0:
+		return x;
+	case 1:
+		return y;
+	case 2:
+		return z;
+	default:
+		std::cerr << "Error : can only acces to a dimension between 0 and 2, tried : " << i << std::endl;
+		return -1;
+	}
+}	
 
 //SETTER
 double& Vector3D::operator[](int i) {
-	if (i >= 0 && i <= 2) {
-		return m_coord[i];
-	}
-	else {
-		std::cerr << "Error : can't acces to a dimension over 3 or below 0 " << i << std::endl;
-		return m_coord[0];
+	switch (i) {
+	case 0:
+		return x;
+	case 1:
+		return y;
+	case 2:
+		return z;
+	default:
+		std::cerr << "Error : can only acces to a dimension between 0 and 2, tried : " << i << std::endl;
+		return x;
 	}
 }
 
@@ -41,64 +44,76 @@ double& Vector3D::operator[](int i) {
 
 //Adding two vectors
 Vector3D Vector3D::operator+(const Vector3D& u) const {	
-  return Vector3D(m_coord[0]+u[0], m_coord[1] + u[1], m_coord[2] + u[2]);
+  return Vector3D(x+u.x, y + u.y, z + u.z);
 }
 
 //Storing the addition of two vectors
 Vector3D& Vector3D::operator+=(const Vector3D& u) {
-  m_coord[0] += u[0];
-  m_coord[1] += u[1];
-  m_coord[2] += u[2];
+  x += u.x;
+  y += u.y;
+  z += u.z;
   return *this;
+}
+
+//return the opposite of this vector
+Vector3D Vector3D::operator-() const {
+	return Vector3D(-x, -y, -z);
 }
 
 //Substract two vectors
 Vector3D Vector3D::operator-(const Vector3D& u) const {
-  return Vector3D(m_coord[0] - u[0], m_coord[1] - u[1], m_coord[2] - u[2]);
+  return Vector3D(x - u.x, y - u.y, z - u.z);
 }
 
 //Storing the substraction of two vectors
 Vector3D& Vector3D::operator-=(const Vector3D& u) {
-  m_coord[0] -= u[0];
-  m_coord[1] -= u[1];
-  m_coord[2] -= u[2];
+  x -= u.x;
+  y -= u.y;
+  z -= u.z;
   return *this;
 }
 
 //Multiplication with a scalar
 Vector3D Vector3D::operator*(double k) const {
-  return Vector3D(m_coord[0]*k, m_coord[1]*k, m_coord[2]*k);
+  return Vector3D(x*k, y*k, z*k);
 }
 
 //Storing the multiplication with a scalar
 Vector3D& Vector3D::operator*=(double k) {
-  m_coord[0] *= k;
-  m_coord[1] *= k;
-  m_coord[2] *= k;
+  x *= k;
+  y *= k;
+  z *= k;
   return *this;
 }
 
 //Multiply each m_coordinate of two vectors
 Vector3D Vector3D::operator*(const Vector3D& u) const {
-  return Vector3D(m_coord[0] * u[0], m_coord[1] * u[1], m_coord[2] * u[2]);
+  return Vector3D(x * u.x, y * u.y, z * u.z);
 }
 
 //Storing the multiplication of each m_coordinate of two vectors
 Vector3D& Vector3D::operator*=(const Vector3D& u) {
-  m_coord[0] *= u[0];
-  m_coord[1] *= u[1];
-  m_coord[2] *= u[2];
+  x *= u.x;
+  y *= u.y;
+  z *= u.z;
   return *this;
+}
+
+//Division by a scalar operators
+Vector3D Vector3D::operator/(double k) const {
+	return Vector3D(x / k, y / k, z / k);
+}
+
+Vector3D& Vector3D::operator/=(double k) {
+	x /= k;
+	y /= k;
+	z /= k;
+	return *this;
 }
 
 //True if two vectors are equals
 bool Vector3D::operator==(const Vector3D& u) const {
-  bool res = true;
-  int i = 0;
-  while (res == true && i < 3 && m_coord[i] == u[i]) {
-    i++;
-  }
-  return res;
+	return (x == u.x && y == u.y && z == u.z);
 }
 
 //True if two vectors are not equals
@@ -122,16 +137,12 @@ double Vector3D::tripleProduct(const Vector3D& u, const Vector3D& v) const {
 
 //Return the scalar product of two vectors
 double Vector3D::scalar(const Vector3D& u) const {
-  double scal = 0;
-  for (int i = 0; i < 3; i++) {
-    scal += m_coord[i] * u[i];
-  }
-  return scal;
+	return (x * u.x + y * u.y + z * u.z);
 }
 
 //Return the norm of the vector
 double Vector3D::norm() const {
-  return std::sqrt(scalar(*this));
+  return std::sqrt(sqrNorm());
 }
 
 //Return the square of the norm of the vector
@@ -143,13 +154,13 @@ double Vector3D::sqrNorm() const {
 Vector3D Vector3D::normalize() const {
   double n = norm();
   if (n == 0) return Vector3D();
-  Vector3D normVec(m_coord[0] / n, m_coord[1] / n, m_coord[2] / n);
+  Vector3D normVec(x / n, y / n, z / n);
   return normVec;
 }
 
 //Return the wedge product of two vectors
 Vector3D Vector3D::cross(const Vector3D& u) const {
-  return Vector3D(m_coord[1]*u[2] - m_coord[2]*u[1], m_coord[2]*u[0] - m_coord[0]*u[2], m_coord[0]*u[1] - m_coord[1]*u[0]);
+  return Vector3D(y*u.z - z*u.y, z*u.x - x*u.z, x*u.y - y*u.x);
 }
 
 
