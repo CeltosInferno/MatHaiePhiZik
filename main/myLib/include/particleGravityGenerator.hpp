1ba278afff9585436cd5fa67f84b4b25152f33ee
nathan barloy <nbarloy@orange.fr>
2019-09-25 15:26:39 -0400

    ajout gravity generator

---
+++ main/myLib/include/particleGravityGenerator.hpp
@@ -0,0 +1,24 @@
+#ifndef PARTICLE_GRAVITY_GENERATOR
+#define PARTICLE_GRAVITY_GENERATOR
+
+#include "particleForceGenerator.hpp"
+
+
+namespace m_engine {
+
+	class ParticleGravityGenerator : public ParticleForceGenerator{
+	public:
+		ParticleGravityGenerator(Vector3D g) : gravity(g) {};
+		~ParticleGravityGenerator() {};
+
+		//generate a force for the particle concerned, time is the duration of a frame (in seconds)
+		void updateForce(Particle& p, double time);
+
+	private :
+		Vector3D gravity;
+
+	};
+
+}
+
+#endif
