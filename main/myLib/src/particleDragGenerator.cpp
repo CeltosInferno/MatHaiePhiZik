c9b54e1131756c374037836cf227e8ee182ce005
nathan barloy <nbarloy@orange.fr>
2019-09-25 15:47:06 -0400

    drag generator

---
+++ main/myLib/src/particleDragGenerator.cpp
@@ -0,0 +1,9 @@
+#include "particleDragGenerator.hpp"
+
+using namespace m_engine;
+
+void ParticleDragGenerator::updateForce(Particle& p, double time) {
+	Vector3D vel = p.getVel();
+	double velNorm = vel.norm();
+	p.addForce(-vel.normalize()*(k1*velNorm + k2*velNorm*velNorm);
+}
