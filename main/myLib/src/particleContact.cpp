f9d79a046b4c49de13b215b7d57435dcad89ea3a
nathan barloy <nbarloy@orange.fr>
2019-10-08 18:07:09 -0400

    correction particleContact : formule + resolution sur les 2 particules

--- main/myLib/src/particleContact.cpp
+++ main/myLib/src/particleContact.cpp
@@ -41,24 +41,28 @@ void ParticleContact::resolve(double time) {
 void ParticleContact::resolveVelocity(double time) {
 	Vector3D vs = -restitutionCoeff * calculVs() * n;
 	if (particles[1] != NULL) { //cas sans sol
-		Vector3D delta = particles[0]->getInversMass() / particles[1]->getInversMass() * vs;
-		particles[0]->setVel(particles[0]->getVel() + delta);
+		double m0 = 1/particles[0]->getInversMass();
+		double m1 = 1 / particles[1]->getInversMass();
+		particles[0]->setVel(particles[0]->getVel() + 2*m1/(m0+m1) * vs);
+		particles[1]->setVel(particles[1]->getVel() - 2*m0/(m0+m1) * vs);
 	}
 	else { // cas du sol
-		//TODO
+		particles[0]->setVel(particles[0]->getVel() + 2 * vs);
 	}
 }
 
 void ParticleContact::resolvePenetration(double time) {
-	Vector3D delta;
 	if (particles[1] == NULL) { //cas du sol : le sol ne bouge pas
-		delta = penetration*n;
+		particles[0]->setPos(particles[0]->getPos() + penetration*n);
 	}
 	else {
 		double ma = 1 / particles[0]->getInversMass();
 		double mb = 1 / particles[1]->getInversMass();
-		delta = mb / (ma + mb) * penetration * n;
+		Vector3D delta0 = mb / (ma + mb) * penetration * n;
+		Vector3D delta1 = -ma / (ma + mb) * penetration * n;
+		particles[0]->setPos(particles[0]->getPos() + delta0);
+		particles[1]->setPos(particles[1]->getPos() + delta1);
 	}
 	
-	particles[0]->setPos(particles[0]->getPos() + delta);
+	
 }
