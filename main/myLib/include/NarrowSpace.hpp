1962e6b553b880ff13087c630ad8616d6be06523
Timothee Corsini <timothee.corsini@gmail.com>
2019-12-04 18:09:45 -0500

    first collision dectection with rbs and planes

---
+++ main/myLib/include/NarrowSpace.hpp
@@ -0,0 +1,44 @@
+#ifndef NARROW_SPACE
+#define NARROW_SPACE
+
+#include "vector3d.hpp"
+#include "rigidBody.hpp"
+#include <vector>
+
+namespace m_engine {
+
+	class Primitive;
+	class Plane;
+
+	struct Contact {
+		Contact(RigidBody* rb, const Vector3D& contactPoint, const Vector3D& contactNormal, float penetration):
+			rb(rb), contactPoint(contactPoint), contactNormal(contactNormal), penetration(penetration)
+		{}
+
+		RigidBody* rb; //the object containing the contactPoint
+		Vector3D contactPoint; //the point where the collision occurs
+		Vector3D contactNormal; //the vector of collision (oriented toward rb in order to use a force)
+		float penetration; //the degree of prenetration
+	};
+
+	struct CollisionData {
+		std::vector<Contact> contacts; //a list of contacts
+		inline void addContact(const Contact& c) {
+			contacts.push_back(c);
+		}
+	};
+
+	class NarrowSpace {
+	public:
+		//given two objects, will return if there is a collision (in narrow space) and stores it in Collision Data
+		static bool solveContact(Primitive*, Primitive*, CollisionData*);
+	private:
+		static bool solveContact(RigidBody*, RigidBody*, CollisionData*);
+		static bool solveContact(RigidBody*, Plane*, CollisionData*);
+	};
+
+
+
+}
+
+#endif /*NARROW_SPACE*/
---
+++ main/myLib/src/narrowSpace.cpp
@@ -0,0 +1,81 @@
+#include "narrowSpace.hpp"
+
+using namespace m_engine;
+
+class m_engine::Primitive {
+public:
+	RigidBody* rigidBody;
+	virtual ~Primitive() {}
+};
+
+class m_engine::Plane : Primitive {
+public:
+	virtual ~Plane() {}
+	Vector3D normal;
+	double offset;
+};
+
+class Box {
+public:
+	Vector3D vertices[8];
+	Box(const RigidBody& rb) {
+		const Vector3D& pos = rb.getPos();
+		double x = rb.dx;
+		double y = rb.dy;
+		double z = rb.dz;
+		//triple boucle for sinon mais relou
+		vertices[0] = pos + Vector3D(x, y, z);
+		vertices[1] = pos + Vector3D(x, y, -z);
+		vertices[2] = pos + Vector3D(x, -y, z);
+		vertices[3] = pos + Vector3D(x, -y, -z);
+		vertices[4] = pos + Vector3D(-x, y, z);
+		vertices[5] = pos + Vector3D(-x, y, -z);
+		vertices[6] = pos + Vector3D(-x, -y, z);
+		vertices[7] = pos + Vector3D(-x, -y, -z);
+	}
+};
+
+bool NarrowSpace::solveContact(Primitive* p1, Primitive* p2, CollisionData* collData) {
+	Plane* plane1 = dynamic_cast<Plane*>(p1);
+	RigidBody* rb1 = p1->rigidBody;
+
+	Plane* plane2 = dynamic_cast<Plane*>(p2);
+	RigidBody* rb2 = p2->rigidBody;
+
+	//the first 
+	if (rb1 == nullptr && rb2 == nullptr) {
+		//no collision handled as there are no rigidBodies
+		return false;
+	}
+	if (rb2 == nullptr && plane2 != nullptr) {
+		//rigidBody1 + Plane2
+		return solveContact(rb1, plane2, collData);
+	}
+	if (rb1 == nullptr && plane1 != nullptr) {
+		//rigidBody2 + Plane1
+		return solveContact(rb2, plane1, collData);
+	}
+	if (rb1 && rb2) {
+		//rigidBody2 + Plane1
+		return solveContact(rb1, rb2, collData);
+	}
+	//should not happen : only if the rb are null but the object is not a plane
+	return false;
+}
+
+bool NarrowSpace::solveContact(RigidBody* rb1, RigidBody* rb2, CollisionData* collData) {
+	return false;
+}
+
+bool NarrowSpace::solveContact(RigidBody* rb, Plane* p, CollisionData* collData) {
+	Box box(*rb);
+	bool isCollided = false;
+	for (const Vector3D& vertice : box.vertices) {
+		double dist = vertice.scalar(p->normal);
+		if (dist < 0) {
+			collData->addContact(Contact(rb, vertice, p->normal, dist));
+			isCollided = true;
+		}
+	}
+	return isCollided;
+}
